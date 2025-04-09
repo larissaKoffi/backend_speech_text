0a2fb5a2b9e52a8fedc5b142d84b5fcda09ec87d
larissaKoffi <larissa.koffi@epitech.eu>
2025-04-09 16:53:46 +0000

    [ADD]: backend correction

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def nettoyer(texte):
     texte = re.sub(r'\s+', ' ', texte).strip() # espaces en trop
     return texte
 
-def summary(text):
+def fn_summary(text):
     inputs = tokenizer(text, return_tensors="pt", max_length=1024, truncation=True)
     summary_ids = model.generate(inputs['input_ids'], max_length=150, num_beams=4, length_penalty=2.0, early_stopping=True)
     return tokenizer.decode(summary_ids[0], skip_special_tokens=True)
@@ -26,7 +26,7 @@ def summarize():
     data = request.get_json()
     texte = data.get('text', '')
     texte_propre = nettoyer(texte)
-    summary = summary(texte_propre)
+    summary = fn_summary(texte_propre)
     return jsonify({'summary': summary})
 
 if __name__ == "__main__":
